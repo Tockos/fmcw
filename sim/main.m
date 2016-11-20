5fab57a5514b898247cd7c6847548de063010e75
tockos <you@example.com>
2016-11-20 22:59:12 +0100

    szarul néz ki, de működik, következik a kód tisztogatása.

--- sim/main.m
+++ sim/main.m
@@ -8,18 +8,15 @@ v_res_exp= 1;
 r_max_exp= 1000;
 r_res_exp= 2;
 
-[B, fc, T, M, N, v_max, v_res, r_max, r_res ]=init(v_max_exp, v_res_exp, r_max_exp, r_res_exp);
+[B, fc, T, gam, M, N, v_max, v_res, r_max, r_res ]=init(v_max_exp, v_res_exp, r_max_exp, r_res_exp);
 [One_chirp, chirp_nos, fs] = gen_chirp(fc, B, T, N, 'none');
 
-Tau_shift=zeros(1, N);
-Smixed=zeros(N, M);
 
 for n= 1 : N
     
     
     [Srec, tau_shift] = gen_rec(One_chirp, n, chirp_nos, T, v, r0);
     [Dec_mix] = mix(One_chirp, Srec, M, chirp_nos);
-    Tau_shift(n)=tau_shift;
     Smixed(n, :)=Dec_mix;
 endfor
 signal_proc(Smixed, N, M);
