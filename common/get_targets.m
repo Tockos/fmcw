fa66406c1a53310461dae65df9251a41eaf00244
tockos <you@example.com>
2016-11-24 15:06:01 +0100

    functionning test files without automatization. filenames are coherent

--- common/get_targets.m
+++ common/get_targets.m
@@ -3,9 +3,9 @@
     tr2=struct;
     tr3=struct;
     tr4=struct;
-    tr1.v=32; tr1.r0=800;
-    tr2.v=-32; tr2.r0=600;
+    tr1.v=50; tr1.r0=15;
+    tr2.v=13; tr2.r0=13;
     tr3.v=-80; tr3.r0=800;
     tr4.v=0; tr4.r0=600;
 
-    targets=[tr1, tr2];
+    targets=[tr1];
