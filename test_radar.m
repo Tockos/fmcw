7ab7df42f2b6c1e9cc6f4a05d14cd3b9ca0630d3
tockos <you@example.com>
2016-11-25 16:15:44 +0100

    the test_radar.m riched two new functions: Err calculation and plot

--- test_radar.m
+++ test_radar.m
@@ -1,13 +1,18 @@
 #!/usr/bin/octave -qf
 
-addpath('modell', 'sim', 'common');
+BASE_PATH="/home/tockos/bme/onlab/fmcw";
+chdir(BASE_PATH);
+addpath('modell', 'sim', 'common', 'test');
 global c B fc T gam M N v_max v_res r_max r_res;   
 
 c=299792458;
+
+IMG_NAME=[argv(){1} "_" argv(){2} "_" argv(){3} "_" argv(){4}];
 r_max_exp=str2num(argv(){1});
 r_res_exp=str2num(argv(){2});
 v_max_exp=str2num(argv(){3});
 v_res_exp=str2num(argv(){4});
+
 
     
 [B, fc, T, gam, M, N, v_max, v_res, r_max, r_res ]=init(v_max_exp, v_res_exp, r_max_exp, r_res_exp);
@@ -21,9 +26,9 @@ v_stat=round(v_max*rand(1, 1));
 
 
 %distance measurement to file
-    fileID = fopen("/home/tockos/bme/onlab/fmcw/temp/r.txt","w");
-    fprintf(fileID,'%15s %30s\n','real distance','measured distance');
-    fprintf(fileID,'==============================================\n');
+    fileID = fopen([BASE_PATH "/temp/r.txt"],"w");
+    fprintf(fileID,'%15s %30s %30s\n','real distance','measured distance', 'Err_r');
+    fprintf(fileID,'============================================================================\n');
 
     for ind=1:length(R_points)
 
@@ -33,19 +38,24 @@ v_stat=round(v_max*rand(1, 1));
         target_rec=modell(target);
         Target_rd=signalproc(target_rec);
         [r_meas, v_meas] = get_measure(Target_rd);
-        
-        fprintf(fileID,'%15d %30d\n', R_points(ind), r_meas);
+       
+       Err_r(ind)=abs(R_points(ind)-r_meas);
+        fprintf(fileID,'%15d %30d %30d\n', R_points(ind), r_meas, Err_r(ind));
         %if(ind==1) break; endif
     endfor
+
+    figure(1)
+    plot(Err_r)
+    saveas(figure(1), [BASE_PATH "/results/images/" "err_r" IMG_NAME ".png"])
 
     fclose(fileID);
 
 
 %velocy measurement to file
 
-    fileID = fopen("/home/tockos/bme/onlab/fmcw/temp/v.txt",'w');
-    fprintf(fileID,'\n\n%15s %30s\n','real velocy','measured velocy');
-    fprintf(fileID,'==============================================\n');
+    fileID = fopen([BASE_PATH "/temp/v.txt"],'w');
+    fprintf(fileID,'\n\n%15s %30s %30s\n','real velocy','measured velocy', 'Err_v');
+    fprintf(fileID,'============================================================================\n');
 
     for ind=1:length(V_points)
 
@@ -55,11 +65,17 @@ v_stat=round(v_max*rand(1, 1));
         target_rec=modell(target);
         Target_rd=signalproc(target_rec);
         [r_meas, v_meas] = get_measure(Target_rd);
+
+        Err_v(ind)=abs(V_points(ind)-v_meas);
         
-        fprintf(fileID,'%15d %30d\n', V_points(ind), v_meas);
+        fprintf(fileID,'%15d %30d %30d\n', V_points(ind), v_meas, Err_v(ind));
 
         %if(ind==1) break; endif
     endfor
+
+    figure(2)
+    plot(Err_v)
+    saveas(figure(2), [BASE_PATH "/results/images/" "err_v" IMG_NAME ".png"])
 
     fclose(fileID);
 
